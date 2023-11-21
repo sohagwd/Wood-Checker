0c668e116a8c5b9770fa11d93f6a370bcd6fb9fd
sohagwd <sohag.wd.bd@gmail.com>
2023-11-22 02:39:18 +0600

    wood app

---
+++ app/src/main/java/com/example/woodchecker/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.woodchecker
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // variable introduction
+        val forInput = findViewById<EditText>(R.id.edInput)
+        val forButton = findViewById<Button>(R.id.btnCheck)
+        val forOutput = findViewById<TextView>(R.id.tvDisplay)
+
+        // btnClick button code here
+        forButton.setOnClickListener {
+            val wood = forInput.text.toString().trim().uppercase().firstOrNull()
+
+            // main code here
+            val result = when(wood){
+                'O' -> "Oak Price = 200 Tk"
+                'P' -> "Pine Price = 350 Tk"
+                'M' -> "Mahogany Price = 980 Tk"
+                'o' -> "Oak Price= 200 Tk"
+                'p' -> "Pine Price= 350 Tk"
+                'm' -> "Mahogany Price= 980 Tk"
+                else -> "Invalid"
+            }
+            forOutput.text = result
+            Toast.makeText(this, result, Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
